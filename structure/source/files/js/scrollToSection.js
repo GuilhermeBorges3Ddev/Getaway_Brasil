ec9a6e29d0519bf858e03bab96a66332efc065e9
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-02-01 22:03:38 -0300

    Script scrollToSection.js created and HTML manipulated to receive these new functionality

---
+++ structure/source/files/js/scrollToSection.js
@@ -0,0 +1,7 @@
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top - "20px"
+    },1200);
+});
