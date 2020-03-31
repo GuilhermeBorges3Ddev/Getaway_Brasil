a5a66c87003f08f0669110e0efe86ddf7ab0b60a
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-03-30 22:55:20 -0300

    Mail sender testing

--- structure/controller/senderContact.php
+++ structure/controller/senderContact.php
@@ -3,27 +3,29 @@
     $subject = $_POST['subject'];
 
     // Contact form fields
-        $name = $_POST['name'];
-        $mail = $_POST['email'];
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
         $phone = $_POST['phone'];
         $message = $_POST['message'];
 
     // Mount body message with these fields
         $body = "<html><body>";
-        $body .= "Nome: $name <br /> Email: $mail <br /> Telefone: $phone <br /> Mensagem: $message";
+        $body .= "Nome: $nome <br \> Email: $email <br \> Telefone: $phone <br \> Mensagem: $message";
         $body .= "</body></html>";
 
     // Mail header
         $email_headers = "MIME-Version: 1.0" . "\r\n";
         $email_headers .= "Content-Type:text/html;charset=UTF-8" . "\r\n";
-        $email_headers .= 'From: ' .$name.'<'.$mail.'>'."\r\n";
+        $email_headers .= 'From: ' .$nome.'<'.$email.'>'."\r\n";
+
 
     // Field verification to send mail
-    if (mail($for, $subject, $body, $email_headers)) {
+    $sender = mail($for, $subject, $body, $email_headers);
+    if ($sender) {
         $msg = "Sua mensagem foi enviada com sucesso. Clique em OK!";
-        echo "<script>alert('$msg');window.location.assign('https://getawaybrasil.com.br/');</script>";
+        echo "<script>alert('$msg');window.location.assign('https://getawaybrasil.com.br');</script>";
     } else {
         $msg = "Erro ao enviar a mensagem, preencha todos os campos. Clique em OK!";
-        echo "<script>alert('$msg');window.location.assign('https://getawaybrasil.com.br/');</script>";
+        echo "<script>alert('$msg');window.location.assign('https://getawaybrasil.com.br');</script>";
     }
 ?>
