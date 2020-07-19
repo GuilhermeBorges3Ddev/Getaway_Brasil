5cd4ce142dec1bfdc540b4d2a6369bf2e49cd43f
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-07-18 22:39:52 -0300

    Subscribe script mod

--- structure/controller/notifyNews.php
+++ structure/controller/notifyNews.php
@@ -1,11 +1,25 @@
 <?php
-    $for = $_POST['email'];
-    $about = "Bem vindo à Getaway Brasil";
-    $html = require('./chimpMail.html');
-    $headers = "Content-type: text/html;\r\n";
+    $forReserva = 'reservas@getawaybrasil.com.br';
+    $for = 'gsilvaborges@gmail.com';
+    $subject = 'Novo usuário inscrito na Getaway para o recebimento de novidades.';
 
-    // Field verification to send notification mail
-    $sender = mail($for, $about, $html, $headers);
+    // Contact form fields
+    $email = $_POST['email'];
+     
+
+    // Mount body message with these fields
+        $body = "<html><body>";
+        $body .= "Email do remetente que deseja receber novidades: $email <br \>";
+        $body .= "</body></html>";
+
+    // Mail header
+        $email_headers = "MIME-Version: 1.0" . "\r\n";
+        $email_headers .= "Content-Type:text/html;charset=UTF-8" . "\r\n";
+        $email_headers .= 'From: <'.$email.'>'."\r\n";
+
+
+    // Field verification to send mail
+    $sender = mail($for, $subject, $body, $email_headers);
     if ($sender) {
         $msg = "Email para recebimento de novidades cadastrado com sucesso. Clique em OK para continuar!";
         echo "<script>alert('$msg');window.location.assign('https://getawaybrasil.com.br');</script>";
